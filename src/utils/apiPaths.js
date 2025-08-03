af541d56019f08456fdeba9e22a1dfcb00ab11c3
Anees khan <aneeskham16202@gmail.com>
2025-08-03 21:08:17 +0500

    Initial commit

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,42 +1,38 @@
-// export const BASE_URL = "http://localhost:8000";
-export const BASE_URL = "https://jobportal-backend-pamy.onrender.com";
+export const BASE_URL = "https://jobportal-backend-kkcj.onrender.com";
 
 export const API_PATHS = {
   AUTH: {
-    REGISTER: "/api/auth/register", // Signup
-    LOGIN: "/api/auth/login", // Authenticate user & return JWT token
-    GET_PROFILE: "/api/auth/profile", // Get logged-in user details
-    UPDATE_PROFILE: "/api/user/profile", // Update profile details
-    DELETE_RESUME: "/api/user/resume", // Delete Resume details
+    REGISTER: "/api/auth/register",
+    LOGIN: "/api/auth/login",
+    GET_PROFILE: "/api/auth/profile",
+    UPDATE_PROFILE: "/api/user/profile",
+    DELETE_RESUME: "/api/user/resume",
   },
 
   DASHBOARD: {
-    OVERVIEW: `/api/analytics/overview`,
+    OVERVIEW: "/api/analytics/overview",
   },
 
   JOBS: {
-    GET_ALL_JOBS: '/api/jobs',
-    GET_JOB_By_ID: (id) => `/api/jobs/${id}`,
-    POST_JOB: "/api/jobs",
-    GET_JOBS_EMPLOYER: "/api/jobs/get-jobs-employer",
+    GET_ALL_JOBS: "/api/jobs",
     GET_JOB_BY_ID: (id) => `/api/jobs/${id}`,
+    POST_JOB: "/api/jobs",
+    GET_JOBS_EMPLOYER: `/api/jobs/get-jobs-employer`,
     UPDATE_JOB: (id) => `/api/jobs/${id}`,
     TOGGLE_CLOSE: (id) => `/api/jobs/${id}/toggle-close`,
     DELETE_JOB: (id) => `/api/jobs/${id}`,
-    DELETE_JOB: (id) => `/api/jobs/${id}`,
-
     SAVE_JOB: (id) => `/api/save-jobs/${id}`,
     UNSAVE_JOB: (id) => `/api/save-jobs/${id}`,
-    GET_SAVED_JOBS: 'api/save-jobs/my',
+    GET_SAVED_JOBS: "/api/save-jobs/my",
   },
 
   APPLICATIONS: {
     APPLY_TO_JOB: (id) => `/api/applications/${id}`,
     GET_ALL_APPLICATIONS: (id) => `/api/applications/job/${id}`,
-    UPDATE_STATUS: (id) => `api/applications/${id}/status`,
+    UPDATE_STATUS: (id) => `/api/applications/${id}/status`,
   },
 
   IMAGE: {
-    UPLOAD_IMAGE: "/api/auth/upload-image", // Upload profile picture
+    UPLOAD_IMAGE: "/api/auth/upload-image",
   },
 };
